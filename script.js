8660ce4137a969d6d75147786dac210159b707da
pryntl <pryntl2007@gmail.com>
2024-07-27 14:28:50 +0330

    build [java script]
    added the list of students and teachers review.

---
+++ script.js
@@ -0,0 +1,130 @@
+let school = {
+    name: "school",
+    classes: [
+      {
+        students: [
+          {
+            name: "asghar",
+            age: 16,
+            number: "09161616161",
+            address: "teh08",
+            terms: [
+              {
+                teachers: {
+                  math: 18,
+                  physics: 19,
+                  chemistery: 17,
+                },
+              },
+              {
+                teachers: {
+                  math: 16,
+                  physics: 18,
+                  chemistery: 19,
+                },
+              },
+              {
+                teachers: {
+                  math: 17,
+                  physics: 20,
+                  chemistery: 17,
+                },
+              },
+            ],
+          },
+          {
+            name: "mmd",
+            age: 17,
+            number: "09171717171",
+            address: "esf04",
+            terms: [
+              {
+                teachers: {
+                  math: 15,
+                  physics: 17,
+                  chemistery: 16,
+                },
+              },
+              {
+                teachers: {
+                  math: 12,
+                  physics: 16,
+                  chemistery: 19,
+                },
+              },
+              {
+                teachers: {
+                  math: 17,
+                  physics: 17,
+                  chemistery: 20,
+                },
+              },
+            ],
+          },
+        ],
+      },
+      {
+        students: [
+          {
+            name: "kolsum",
+            age: 18,
+            number: "09181818181",
+            address: "shz09",
+            terms: [
+              {
+                teachers: {
+                  math: 18,
+                  physics: 14,
+                  chemistery: 16,
+                },
+              },
+              {
+                teachers: {
+                  math: 19,
+                  physics: 15,
+                  chemistery: 17,
+                },
+              },
+              {
+                teachers: {
+                  math: 18.5,
+                  physics: 13,
+                  chemistery: 18,
+                },
+              },
+            ],
+          },
+          {
+            name: "azam",
+            age: 19,
+            number: "09191919191",
+            address: "bu03",
+            terms: [
+              {
+                teachers: {
+                  math: 20,
+                  physics: 19,
+                  chemistery: 15,
+                },
+              },
+              {
+                teachers: {
+                  math: 19.25,
+                  physics: 18.5,
+                  chemistery: 17,
+                },
+              },
+              {
+                teachers: {
+                  math: 19,
+                  physics: 19.75,
+                  chemistery: 17,
+                },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  console.log(school);
